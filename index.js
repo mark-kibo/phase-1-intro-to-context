d25f9838b1291d2887152fc18896ed2b576d6174
mark-kibo <kibochamark@gmail.com>
2023-07-29 23:15:40 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,22 +1,12 @@
-// Your code here
 function createEmployeeRecord(array){
-    let result={}
-    for (let key in array){
-        if(key == 0){
-            result["firstName"]= array[key]
-        }else if(key == 1){
-            result["familyName"]= array[key]
-        }else if(key == 2){
-            result["title"]= array[key]
-        }else if( key == 3){
-            result["payPerHour"]= array[key]
-        }
+    let result={
+        "firstName": array[0],
+        "familyName":array[1],
+        "title":array[2],
+        "payPerHour": array[3],
+        "timeInEvents":[],
+        "timeOutEvents":[]
     }
-    let timeInEvents=[]
-    let timeOutEvents=[]
-    result=Object.assign(result, {"timeInEvents": timeInEvents},{"timeOutEvents": timeOutEvents})
-
-
     return result;
 
 }
@@ -27,143 +17,134 @@ function createEmployeeRecords(arrays){
 
     arrays.forEach(element => {
         let data=createEmployeeRecord(element)
-        // console.log(data)
         result.push(data)
     });
-    // console.log(result)
     return result;
 }
 
-// console.log(createEmployeeRecords([["kibo", "new"], ["zippy", "leo"]]))
-function createTimeInEvent(employeeRecord, dateStamp){
-    const year = dateStamp.slice(0, 4);
-    const month = dateStamp.slice(5, 7); // month is zero-indexed
-    const day = dateStamp.slice(8, 10);
-    const hours = dateStamp.slice(11, 13);
-    const minutes = dateStamp.slice(13, 15);
+
+function createTimeInEvent(employeeRecord, datestamp){
+    const year = datestamp.slice(0, 4);
+    const month = datestamp.slice(5, 7);
+    const day = datestamp.slice(8, 10);
+    const hours = datestamp.slice(11, 13);
+    const minutes = datestamp.slice(13, 15);
+
+    // create date object
+    const dateObject = new Date(year, month - 1, day, hours, minutes);
     
-    const dateObject = new Date(year, month, day, hours, minutes);
+    
+    //  extract our date and hour from dateObject
+    let date = `${year}-${month}-${day}`;
+    let hour = dateObject.toLocaleString('en-US', { hour12: false, hour: 'numeric', minute: 'numeric' }).replace(":" , "");
+
+    // access timeevents in employee record and pass our timeInobject
+    let timeInDetails={
+        "type": "TimeIn",
+        "hour": parseInt(hour),
+        "date": date
+    }
+    employeeRecord.timeInEvents.push(timeInDetails)
+
+    return employeeRecord
+}
+
+function createTimeOutEvent(employeeRecord, datestamp){
+    const year = datestamp.slice(0, 4);
+    const month = datestamp.slice(5, 7);
+    const day = datestamp.slice(8, 10);
+    const hours = datestamp.slice(11, 13);
+    const minutes = datestamp.slice(13, 15);
+
+    // create date object
+    const dateObject = new Date(year, month - 1, day, hours, minutes);
+    
+    
+    //  extract our date and hour from dateObject
+    let date = `${year}-${month}-${day}`;
+    let hour = dateObject.toLocaleString('en-US', { hour12: false, hour: 'numeric', minute: 'numeric' }).replace(":" , "");
+
+    // access timeevents in employee record and pass our timeInobject
+    let timeOutDetails={
+        "type": "TimeOut",
+        "hour": parseInt(hour),
+        "date": date
+    }
+    employeeRecord.timeOutEvents.push(timeOutDetails)
+
+    return employeeRecord
+}
+
+
+function hoursWorkedOnDate(employeeRecord, dateString){
+    // "00044-13-15"
+    let timeInHour;
+    let timeOutHour
+
+
+    // get employee time in and time out that match the date specified
+    employeeRecord.timeInEvents.forEach(element => {
+        if(element["date"] === dateString) timeInHour= element["hour"]
+        
+    });
+    employeeRecord.timeOutEvents.forEach(timeOutEvent=>{
+        if(timeOutEvent["date"] === dateString) timeOutHour= timeOutEvent["hour"]
+     })
+
+
+
+    // calculate hours worked,  by extracting hours and minutes depending on the result.
+    // the result can have a length of 3 or 4 meaning , you can minus hours and get eg 1200 which means 12 hours and 0 minutes
+// or 200 which means 2hours and 0 minutes 
+    let hoursWorked= timeOutHour-timeInHour
+    if(hoursWorked.toString().length == 3){
+        hoursWorked=parseInt(hoursWorked.toString().slice(0,1)) + (parseInt(hoursWorked.toString().slice(1))/60)
+    }else if(hoursWorked.toString().length == 4){
+        hoursWorked=parseInt(hoursWorked.toString().slice(0,2)) + (parseInt(hoursWorked.toString().slice(3))/60)
+    }
+    
+    return hoursWorked
    
-    let list=employeeRecord.timeInEvents
-    
-    let newHour = dateObject.getHours()
-    let newYear =dateObject.getFullYear();
-    let newMonth = dateObject.getMonth() + 1; // Adding 1 because getMonth() returns values from 0 to 11
-    let newDay = dateObject.getDate();
-    let date = `${year}-${month}-${day}`;
-
-    list.push({"type": "TimeIn","hour" :newHour,"date": date})
-
-    return employeeRecord
-}
-
-// console.log(createEmployeeRecords([["kibo", "new"], ["zippy", "leo"]]))
-function createTimeOutEvent(employeeRecord, dateStamp){
-    const year = dateStamp.slice(0, 4);
-    const month = dateStamp.slice(5, 7); // month is zero-indexed
-    const day = dateStamp.slice(8, 10);
-    const hours = dateStamp.slice(11, 13);
-    const minutes = dateStamp.slice(13, 15);
-
-    const dateObject = new Date(year, month, day, hours, minutes);
-
-    let list=employeeRecord.timeOutEvents
-    let newHour = dateObject.getHours();
-
-    let newYear =dateObject.getFullYear();
-    let newMonth = dateObject.getMonth() + 1; // Adding 1 because getMonth() returns values from 0 to 11
-    let newDay = dateObject.getDate();
-    let date = `${year}-${month}-${day}`;
-    console.log(newHour, newYear, newDay, date)
-    list.push({"type": "TimeOut","hour" :newHour,"date": date})
-
-    return employeeRecord
 }
 
 
-function hoursWorkedOnDate(employeeRecord,datestamp){
-    let records =createEmployeeRecord(employeeRecord)
-    let updatedBpRecord = createTimeInEvent(records, datestamp)
-    let updatedBpRecordOne = createTimeOutEvent(records, datestamp)
-    let list = updatedBpRecord.timeInEvents
-    let list2 = updatedBpRecordOne.timeOutEvents
+function wagesEarnedOnDate(employeeRecord, datestamp){
+    let hoursWorked=hoursWorkedOnDate(employeeRecord, datestamp)
+    let payPerHour=employeeRecord.payPerHour
 
-    let inHour;
-    let outHour;
+    // return wage = time taken * pay per hour
+    return hoursWorked * payPerHour
 
-    list.forEach(data=>{
-       
-        if(Object.values(data).includes(datestamp)){
-            inHour=data.hour
+}
+
+
+function allWagesFor(employeeRecord){
+    let wages=[]
+    employeeRecord.timeInEvents.forEach(event=>{
+        let date=""
+        if(event["date"] != date){
+            date=event["date"]
+            let pay = wagesEarnedOnDate(employeeRecord, date)
+            wages.push(pay)
         }
     })
-    
-    
-    list2.forEach(data=>{
-        if(Object.values(data).includes(datestamp)){
-            outHour=data.hour
-            }
-    })
 
-    return outHour - inHour
-    
-
-}
-
-
-function wagesEarnedOnDate(employeeRecord,datestamp){
-    let hours = hoursWorkedOnDate(employeeRecord, datestamp)
-    let payRate=createEmployeeRecord(employeeRecord).payPerHour
-
-    return payRate * hours
-
-
-}
-
-function allWagesFor(employeeRecords){
-    let records=createEmployeeRecords(employeeRecords)
-    let hours = []
-    records.forEach(element=>{
-        let wagesEarned=wagesEarnedOnDate(element, element.timeInEvents.date)
-        hours.push(wagesEarned)
-    })
-
-    let result = hours.reduce((accumulator, currentValue) => accumulator + currentValue)
+    let result=wages.reduce((accumulator, currentValue)=> accumulator + currentValue)
 
     return result
 }
 
-function findEmployeeByFirstName(employeeRecords, myFirstName){
-    let employees=createEmployeeRecords(employeeRecords)
-    console.log(typeof(myFirstName))
-    let employee = employees.find((data) =>{
-        if(data["firstName"] == myFirstName){
-            return data
-        }
+
+function calculatePayroll(employees){
+    let result = []
+    employees.forEach(employee =>{
+        result.push(allWagesFor(employee))
     })
-    console.log(employee)
+
+    return result.reduce((accumulator, currentValue)=> accumulator + currentValue)
 }
 
-findEmployeeByFirstName([["Julius", "Caesar", "General", 27], ["Byron", "Poodle", "Mascot", 3]], "Byron")
 
-function calculatePayroll(employeeRecords){
-    let employees=createEmployeeRecords(employeeRecords)
-    employees.forEach(employee=>{
-        date1String= `${employee.timeInEvents[0].date} ${employee.timeInEvents[0].hour}`
-        date2String= `${employee.timeOutEvents[0].date} ${employee.timeOutEvents[0].hour}`
-        const date1 = new Date(date1String);
-        const date2 = new Date(date2String);
-    
-        const timeDiffInMs = Math.abs(date2 - date1);
-        const timeDiffInHours = timeDiffInMs / 3600000;
-    })
-   
 
-    const date1 = new Date(date1String);
-    const date2 = new Date(date2String);
 
-    const timeDiffInMs = Math.abs(date2 - date1);
-    const timeDiffInHours = timeDiffInMs / 3600000;
 
-    console.log(timeDiffInHours); // outputs 3.5
-}
